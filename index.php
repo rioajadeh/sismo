14b4fc51a83332b517308e2cfbf846e7b5174d18
rioajadeh <riomuhf@gmail.com>
2025-05-14 09:05:43 +0700

    tampilan diperbagus

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Data Pendaftaran</title>
+
+    <!-- tampilan -->
     <style>
         body {
             background-color: #f4f6f9;
